75e1911d9dce3c716c624ad96b017c938c284ce0
Conan Rimmer <conan@Conans-MacBook-Pro.local>
2011-12-15 14:53:52 -0600

    brand new stuff peeps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'd7b0c2a2377303edd7a0289e5f4030b7419ee5f1916e2678e6ddb25dbe095be6ed91d8871f20fd180878f5159ffe576e6f0b7b39ff7f52d8bdba5552d4e28477'
